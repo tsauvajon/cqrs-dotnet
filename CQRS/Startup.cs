0370804e15a23743fcece31ab022bf9042d0d6ed
Thomas Sauvajon <thomas.sauvajon@gmail.com>
2017-08-02 17:58:25 +0200

    Added a web api, configured asp.net Core for DI

--- CQRS/Startup.cs
+++ CQRS/Startup.cs
@@ -7,21 +7,43 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using StructureMap;
+using FluentValidation;
+using StackExchange.Redis;
+using CQRS.Employee;
+using CQRS.Location;
+using Microsoft.Extensions.Configuration;
 
 namespace CQRS
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add framework services.
+            services.AddMvc();
+            services.AddRouting();
+        }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
 
             if (env.IsDevelopment())
             {
@@ -32,6 +54,47 @@ namespace CQRS
             {
                 await context.Response.WriteAsync("Hello World!");
             });
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}");
+            });
+        }
+
+        /// <summary>
+        /// Configure StructureMap ?
+        /// </summary>
+        /// <param name="services">Services to configure</param>
+        /// <returns>Service provider</returns>
+        public IServiceProvider ConfigureIoC(IServiceCollection services)
+        {
+            var container = new Container();
+
+            container.Configure(config =>
+            {
+                config.Scan(_ =>
+                {
+                    _.AssemblyContainingType(typeof(Startup));
+                    _.WithDefaultConventions();
+                    _.ConnectImplementationsToTypesClosing(typeof(IValidator<>));
+                });
+
+                // Repos
+                config.For<IEmployeeRepository>().Use<EmployeeRepository>();
+                config.For<ILocationRepository>().Use<LocationRepository>();
+
+                //StackExchange.Redis
+                ConnectionMultiplexer multiplexer = ConnectionMultiplexer.Connect("localhost");
+                config.For<IConnectionMultiplexer>().Use(multiplexer);
+
+                //config.Populate(services);
+            });
+
+            return container.GetInstance<IServiceProvider>();
         }
     }
 }
